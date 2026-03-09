12fa853f9747437a2617a3b3616509139b512549
rexkraemer <felix.schrader@posteo.de>
2026-03-09 16:18:55 +0100

    sql update

---
+++ sql/create_tables.sql
@@ -0,0 +1,31 @@
+-- Datenbank erstellen (in pgAdmin oder psql)
+CREATE DATABASE olympics;
+
+-- Tabellen
+CREATE TABLE athletes (
+    athlete_id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    sex CHAR(1)
+);
+
+CREATE TABLE countries (
+    noc CHAR(3) PRIMARY KEY,
+    country VARCHAR(255)
+);
+
+CREATE TABLE events (
+    event_id SERIAL PRIMARY KEY,
+    sport VARCHAR(255),
+    event VARCHAR(255)
+);
+
+CREATE TABLE athlete_event (
+    athlete_id INT REFERENCES athletes(athlete_id),
+    event_id INT REFERENCES events(event_id),
+    noc CHAR(3) REFERENCES countries(noc),
+    year INT,
+    age INT,
+    height FLOAT,
+    weight FLOAT,
+    medal VARCHAR(10)
+);
